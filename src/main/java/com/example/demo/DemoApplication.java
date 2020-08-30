d2932155821484157c56d56200e3a719639f6072
Leandro Alves <leandrorralves@gmail.com>
2020-08-30 19:29:44 -0300

    RESTful Web Service
    RESTful Web Service
    Based on the following tutorial: https://spring.io/guides/gs/rest-service/

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restservice.Greeting;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +21,15 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	// Objeto retorna um JSON, usa implicitamente Jackson 2
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 }
